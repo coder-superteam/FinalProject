69ac4104eb3e175746c720927415bbc8edce689d
Nguyen Anh Thi <nguyenanhthi@gogojungle.vn>
2017-03-20 00:24:20 +0700

    User can upload avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :ensure_signup_complete, only: [:new, :create, :update, :destroy]
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:username, :email, :password, :current_password, :avatar)
+    end
+  end
+
+  # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth, :avatar) }
+
   def ensure_signup_complete
     return if action_name == "finish_signup"
     if current_user && !current_user.email_verified?

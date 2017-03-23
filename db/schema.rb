9836286341d0447e3c2dcdcd74eed205ec7e7be9
hnanhquoc <hnanhquoc@gmail.com>
2017-03-23 22:21:10 +0700

    add user to room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323063919) do
+ActiveRecord::Schema.define(version: 20170323151757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170323063919) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
